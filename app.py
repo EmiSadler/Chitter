c826878300d36d42e9bb995ad1b4cdd8e0688ae2
Emily Sadler <emily@sadler.gg>
2025-01-17 13:28:34 +0000

    updated the secret key and added dockerfile

--- app.py
+++ app.py
@@ -97,7 +97,9 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
+    app.secret_key = os.environ.get('SECRET_KEY', 'super secret key')
     # app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)), host="0.0.0.0")
+
+
 
